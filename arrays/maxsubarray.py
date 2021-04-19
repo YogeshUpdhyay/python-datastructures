7009bd2e698d715113ab4a1e489b501747631d3f
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-19 20:50:41 +0530

    kadanes problem added

---
+++ arrays/maxsubarray.py
@@ -0,0 +1,20 @@
+# to find the max sum for a contiguos subarray of the give array
+
+def maxSubArraySum(a,size):
+      
+    max_so_far = 0
+    max_ending_here = 0
+      
+    for i in range(0, size):
+        max_ending_here = max_ending_here + a[i]
+        if (max_so_far < max_ending_here):
+            max_so_far = max_ending_here
+ 
+        if max_ending_here < 0:
+            max_ending_here = 0  
+    return max_so_far
+
+if __name__ == '__main__':
+    arr = list(map(int, input().strip().split()))
+    max_sum = maxSubArraySum(arr, len(arr))
+    print(max_sum)
