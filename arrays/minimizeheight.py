13aa8e65587a909ab107acd36cd32e390434d182
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-20 18:30:15 +0530

    minimize the heigth

--- arrays/minimizeheight.py
+++ arrays/minimizeheight.py
@@ -1,54 +1,42 @@
+# two pointer method
 def getMinDiff(arr, n, k):
     # code here
-    v = []
-    taken = [0]*n
-    
-    for i in range(n):
-        if arr[i]-k >= 0:
-            v.append([arr[i]-k,i])
-        v.append([arr[i] + k,i])
-        
-    
-    v.sort()
-    
-    elements_in_range = 0;
-    left = 0
-    right = 0
-    
-    while elements_in_range < n and right < len(v) :
-        
-        if taken[v[right][1]] == 0 :
-            elements_in_range+=1;
-        
-        taken[v[right][1]]+=1;
-        right+=1;
-    
-    
-    ans = v[right - 1][0] - v[left][0]
-    
-    while right < len(v) :
+    if (n == 1):
+        return 0
 
-        if(taken[v[left][1]] == 1) :
-            elements_in_range-=1;
-        
-        taken[v[left][1]]-=1;
-        left+=1
-        
-        while elements_in_range < n and right < len(v) : 
-            if taken[v[right][1]] == 0 :
-                elements_in_range+=1
-            
-            taken[v[right][1]]+=1
-            right+=1
-        
-        
-        if(elements_in_range == n) : 
-            ans = min(ans, v[right - 1][0] - v[left][0])
-        else :
-            break
-        
-    
-    return ans
+    arr.sort()
+
+    ans = arr[n-1] - arr[0]
+
+    small = arr[0] + k
+    big = arr[n-1] - k
+
+    if (small > big):
+        small, big = big, small
+ 
+    # Traverse middle elements
+    for i in range(1, n-1):
+     
+        subtract = arr[i] - k
+        add = arr[i] + k
+ 
+        # If both subtraction and addition
+        # do not change diff
+        if (subtract >= small or add <= big):
+            continue
+ 
+        # Either subtraction causes a smaller
+        # number or addition causes a greater
+        # number. Update small or big using
+        # greedy approach (If big - subtract
+        # causes smaller diff, update small
+        # Else update big)
+        if (big - subtract <= add - small):
+            small = subtract
+        else:
+            big = add
+ 
+    return min(ans, big - small)
 
 if __name__ == '__main__':
     arr = list(map(int, input().strip().split()))
