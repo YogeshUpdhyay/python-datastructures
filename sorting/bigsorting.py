32c40d71f8a071d198c66c289c992812b764aa88
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-04-24 19:40:45 +0530

    big sorting

--- sorting/bigsorting.py
+++ sorting/bigsorting.py
@@ -13,9 +13,30 @@ import sys
 # The function accepts STRING_ARRAY unsorted as parameter.
 #
 
+# design a class with treats string as integer and replace the integers methods 
+# with comparision dunder methods 
+class StringAsInt(object):
+    def __init__(self, obj, *args):
+        self.obj = obj
+    def __lt__(self, other):
+        if len(self.obj) != len(other.obj):
+            return len(self.obj) < len(other.obj)
+        else:
+            return self.obj < other.obj
+    def __gt__(self, other):
+        return other < self
+    def __eq__(self, other):
+        return self.obj == other.obj
+    def __le__(self, other):
+        return not (self > other)
+    def __ge__(self, other):
+        return (other <= self)
+    def __ne__(self, other):
+        return self.obj != other.obj
+
 def bigSorting(unsorted):
     # Write your code here
-    sortarr = sorted(unsorted, key=int)
+    sortarr = sorted(unsorted, key=StringAsInt)
     return sortarr
 
 if __name__ == '__main__':
