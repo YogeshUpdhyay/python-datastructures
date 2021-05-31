e155438ba23528edcf055b70da42aeeec2e7ce06
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-05-31 22:44:34 +0530

    coin change problem

---
+++ dynamic-programming/coinchange.py
@@ -0,0 +1,32 @@
+import sys
+
+def minCoins(n, arr, dp):
+	if n == 0:
+		return 0
+
+	ans = sys.maxsize
+
+	for i in arr:
+		if n-i >= 0:
+			if dp[n-1] != -1:
+				subans = dp[n-i]
+			else:
+				subans = minCoins(n-i, arr, dp)
+			if (subans != sys.maxsize and subans+1 < ans):
+				ans = subans + 1
+	dp[n] = ans
+	return ans
+
+
+if __name__ == '__main__':
+	n = int(input().strip())
+
+	arr = list(map(int, input().split()))
+
+	dp = [-1]*(n+1)
+	dp[0] = 0
+
+	result = minCoins(n, arr, dp)
+	
+	print(result)
+
