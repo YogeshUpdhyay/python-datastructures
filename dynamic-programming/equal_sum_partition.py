353cafbb941c82cc5bd98c89c7b63230d9f72eae
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-07-02 17:49:02 +0530

    equal sum partition

--- dynamic-programming/equal_sum_partition.py
+++ dynamic-programming/equal_sum_partition.py
@@ -1,5 +1,31 @@
+
+def isSubset(n, arr):
+    dp = [[False]*(n+1) for _ in range(len(arr)+1)]
+    for i in dp:
+        i[0] = True
+
+    i = 1
+    while i < len(arr)+1:
+        j = 1
+        while j < n+1:
+            if j < arr[i-1]:
+                dp[i][j] = dp[i-1][j]
+            else:
+                if dp[i-1][j] or dp[i-1][j-arr[i-1]]:
+                    dp[i][j] = True
+                else:
+                    dp[i][j] = False
+            j += 1
+        i += 1
+    
+    return dp[i-1][j-1]
+
 def isEqualSumPartition(arr):
-    pass
+    if sum(arr)%2 != 0:
+        return False
+    
+    return(isSubset(sum(arr)//2, arr))
+
 
 if __name__ == '__main__':
     arr = list(map(int, input().strip().split(" ")))
