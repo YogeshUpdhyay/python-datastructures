cfae9121caae3775f9d439553579bc92948d5c41
Yogesh Upadhyay <yogeshh2021@gmail.com>
2021-09-24 23:23:31 +0530

    Character Replacement

--- arrays/characterReplacement.py
+++ arrays/characterReplacement.py
@@ -1,15 +1,18 @@
-def characterReplacement( s: str, k: int) -> int:
-    maxSubstr = 0
-    n = len(s)
-    for i in range(n):
-        anchor = s[i]
-        changes = 0
-        j = i+1
-        while j < n and changes < k:
-            if s[j] != anchor:
-                changes += 1
-            j+=1
-        maxSubstr = max(maxSubstr, j - i)
-    return maxSubstr
+def characterReplacement(s: str, k: int) -> int:
+    chars = {}
+    left, maxf, output = 0, 0, 0
+    for right in range(len(s)):
+        char = s[right]
+        
+        chars[char] = 1 + (chars.get(char, 0))
+        maxf = max(maxf, chars[char])
+        
+        while (right-left+1) - maxf > k:
+            chars[s[left]] -= 1
+            left += 1
+            
+        output = max(output, right-left+1)
+    return output
+
 
 print(characterReplacement("AABABBA", 1))
