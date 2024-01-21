7e7566d5c12d63a30e5f7339950d0ef6b65a7d5f
Yogesh Upadhyay <yogeshh2021@gmail.com>
2024-01-21 14:27:36 +0530

    Update dfs

--- arrays/bfs/rottenOranges.py
+++ arrays/bfs/rottenOranges.py
@@ -1,58 +1,106 @@
-def orangesRotting(grid):
-    nrows = len(grid)
-    ncols = len(grid[0])
+# def orangesRotting(grid):
+#     nrows = len(grid)
+#     ncols = len(grid[0])
 
-    q = []
-    nOfOne = 0
-    for i in range(nrows):
-        for j in range(ncols):
-            if grid[i][j] == 2:
-                q.append([i, j])
+#     q = []
+#     nOfOne = 0
+#     for i in range(nrows):
+#         for j in range(ncols):
+#             if grid[i][j] == 2:
+#                 q.append([i, j])
 
-            if grid[i][j] == 1:
-                nOfOne += 1
+#             if grid[i][j] == 1:
+#                 nOfOne += 1
 
-    q.append(None)
+#     q.append(None)
 
-    time = 0
+#     time = 0
 
-    while q and q != [None]:
-        s = q.pop(0)
+#     while q and q != [None]:
+#         s = q.pop(0)
 
-        if s is None:
+#         if s is None:
+#             time += 1
+#             q.append(None)
+#         else:
+#             # Top
+#             if s[0]-1 >= 0 and grid[s[0]-1][s[1]] == 1:
+#                 grid[s[0]-1][s[1]] = 2
+#                 q.append([s[0]-1, s[1]])
+#                 nOfOne -= 1
+
+#             # Down
+#             if s[0]+1 < nrows and grid[s[0]+1][s[1]] == 1:
+#                 grid[s[0]+1][s[1]] = 2
+#                 q.append([s[0]+1, s[1]])
+#                 nOfOne -= 1
+
+#             # Right
+#             if s[1]+1 < ncols and grid[s[0]][s[1]+1] == 1:
+#                 grid[s[0]][s[1]+1] = 2
+#                 q.append([s[0], s[1]+1])
+#                 nOfOne -= 1
+
+#             # Left
+#             if s[1]-1 >= 0 and grid[s[0]][s[1]-1] == 1:
+#                 grid[s[0]][s[1]-1] = 2
+#                 q.append([s[0], s[1]-1])
+#                 nOfOne -= 1
+
+#     if nOfOne > 0:
+#         return -1
+#     else:
+#         return time
+
+
+# n = int(input())
+# grid = [list(map(int, input().split())) for i in range(n)]
+# print(orangesRotting(grid))
+
+
+class Solution:
+    def orangesRotting(self, grid) -> int:
+        q = []
+        time, fresh = 0, 0
+
+        ROWS, COLS = len(grid), len(grid[0])
+
+        # preprocessing
+        # this creates the initial queue for the bfs algo and keep a count of 
+        # the fresh oranges to see if all the fresh ranges after the bfs have becom rotten or not
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1:
+                    fresh += 1
+
+                if grid[r][c] == 2:
+                    q.append([r, c])
+
+        
+
+        # bfs logic
+        directions = [[0, 1], [0, -1], [1,0], [-1, 0]]
+        while q and fresh > 0:
+
+            for i in range(len(q)):                
+                r, c = q.pop(0)
+                for dr, dc in directions:
+
+                    row, col = dr + r, dc + c
+
+                    # out of bounds condition and if not a fresh orange
+                    if row < 0 or col < 0 or row == ROWS or col == COLS or grid[row][col] != 1:
+                        continue
+
+
+                    grid[row][col] = 2
+                    q.append([row, col])
+                    fresh -= 1
+
             time += 1
-            q.append(None)
-        else:
-            # Top
-            if s[0]-1 >= 0 and grid[s[0]-1][s[1]] == 1:
-                grid[s[0]-1][s[1]] = 2
-                q.append([s[0]-1, s[1]])
-                nOfOne -= 1
 
-            # Down
-            if s[0]+1 < nrows and grid[s[0]+1][s[1]] == 1:
-                grid[s[0]+1][s[1]] = 2
-                q.append([s[0]+1, s[1]])
-                nOfOne -= 1
+        
+        return time if fresh == 0 else -1
 
-            # Right
-            if s[1]+1 < ncols and grid[s[0]][s[1]+1] == 1:
-                grid[s[0]][s[1]+1] = 2
-                q.append([s[0], s[1]+1])
-                nOfOne -= 1
-
-            # Left
-            if s[1]-1 >= 0 and grid[s[0]][s[1]-1] == 1:
-                grid[s[0]][s[1]-1] = 2
-                q.append([s[0], s[1]-1])
-                nOfOne -= 1
-
-    if nOfOne > 0:
-        return -1
-    else:
-        return time
-
-
-n = int(input())
-grid = [list(map(int, input().split())) for i in range(n)]
-print(orangesRotting(grid))
+sol = Solution()
+print(sol.orangesRotting([[1],[2],[1],[2]]))
--- temp.py
+++ temp.py
@@ -1,22 +1,57 @@
 
-def max_score(numbers, N):
+import sys
+import heapq
 
-    def gcd(a, b):
-        # Everything divides 0
-        if (b == 0):
-            return a
-        return gcd(b, a % b)
-
-    def lcm(a, b):
-        return (a / gcd(a, b)) * b
-
-    gcd_a = numbers[0][0]
-    gcd_b = numbers[0][1]
-    for i in range(N):
-        gcd_a = gcd(gcd_a, numbers[i][0])
-        gcd_b = gcd(gcd_b, numbers[i][1])
-
-    return int(lcm(gcd_a, gcd_b))
-
-
-print(max_score([[2, 10], [5, 15]], 2))
+class Solution:
+    def minimumCost(self, nums, k: int, dist: int) -> int:
+        # except the first element heapify all the rest element with their indexes
+        h = []
+        for i in range(1, len(nums)):
+            heapq.heappush(h, (nums[i], i))
+            
+        # we need to get the first k element
+        min_idx = sys.maxsize
+        max_idx = -sys.maxsize
+        partitions = []
+        while k > 1:
+            partition = heapq.heappop(h)
+            min_idx = min(min_idx, partition[1])
+            max_idx = max(max_idx, partition[0])
+            partitions.append(partition)
+            k -= 1
+            
+        while abs(min_idx - max_idx) > dist:
+            partitions.pop()
+            partition = heapq.heappop(h)
+            min_idx = min(min_idx, partition[1])
+            max_idx = max(max_idx, partition[0])
+            partitions.append(partition)
+        
+        
+        # once this loop is done we have the partiitons 
+        # partitions.sort(key=lambda x: x[1])
+        # while abs(partitions[0][1] - partitions[-1][1]) > dist:
+        #     # remove the max value partition
+        #     partitions.sort()
+        #     partitions.pop()
+        #     partition = heapq.heappop(h)
+        #     partitions.append(partition)
+        #     partitions.sort(key=lambda x: x[1])
+            
+        # calculate cost
+        cost = nums[0]
+        for i in partitions:
+            cost += i[0]
+             
+        return cost
+        
+        
+sol = Solution()
+tc = [
+    [[1,3,2,6,4,2], 3,3],
+    [[10,1,2,2,2,1], 4, 3],
+    [[10,8,18,9], 3,1],
+    [[1,6,4,7,9,6,1], 4, 4]
+]
+for t in tc:
+    print(sol.minimumCost(*t))
